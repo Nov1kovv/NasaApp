93fb10c079e3c7817ecdaf43d2dd2f982fcfe10f
Oleg Novikov <novikov7@yahoo.com>
2025-02-04 12:03:00 +0300

    stash

---
+++ app/src/main/java/com/example/nasaapp/ui/search/SearchFragment.kt
@@ -0,0 +1,28 @@
+package com.example.nasaapp.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nasaapp.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        val searchItems = List(100) { SearchItem("${it + 1}",
+            "https://avatars.mds.yandex.net/i?id=c73ba79f654009e2cac2a6e885c19368_l-5334002-images-thumbs&n=13",
+            "03.02.2025")}
+        val recyclerView: RecyclerView? = view?.findViewById(R.id.recycler_view)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        recyclerView?.adapter = SearchAdapter(searchItems)
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/nasaapp/ui/search/SearchItem.kt
@@ -0,0 +1,3 @@
+package com.example.nasaapp.ui.search
+
+data class SearchItem(val name: String, val imageUrl: String, val date: String)
