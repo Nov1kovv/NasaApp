c96fef349a3609a631949b2481ca652c98585dae
Oleg Novikov <novikov7@yahoo.com>
2025-02-14 11:57:34 +0300

    DetailFragment

--- app/src/main/java/com/example/nasaapp/ui/search/DetailFragment.kt
+++ app/src/main/java/com/example/nasaapp/ui/search/DetailFragment.kt
@@ -5,15 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.VideoView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.nasaapp.R
 import com.example.nasaapp.domain.model.SearchItem
 
 class DetailFragment : Fragment() {
 
-    private lateinit var videoView: VideoView
+    private lateinit var imageView: ImageView
     private lateinit var fileSizeTextView: TextView
     private lateinit var fileFormatTextView: TextView
     private lateinit var nasaIdTextView: TextView
@@ -28,7 +29,7 @@ class DetailFragment : Fragment() {
         searchItem = arguments?.getParcelable("searchItem")!!
         val view = inflater.inflate(R.layout.detail_fragment, container, false)
 
-        videoView = view.findViewById(R.id.video_view)
+        imageView = view.findViewById(R.id.image_view)
         fileSizeTextView = view.findViewById(R.id.file_size)
         fileFormatTextView = view.findViewById(R.id.file_format)
         nasaIdTextView = view.findViewById(R.id.nasa_id)
@@ -38,11 +39,19 @@ class DetailFragment : Fragment() {
         fileFormatTextView.text = "Format: MP4"
         nasaIdTextView.text = "NASA ID: ABC123XYZ"
 
+        val imageUrl = "https://imgpng.ru/img/heroes/spongebob/49737.png"
+        Glide.with(this)
+            .load(imageUrl)
+            .into(imageView)
+
 
         downloadButton.setOnClickListener {
-
+//            downloadVideo("android.resource://${activity?.packageName}/${R.raw.why_does_the_moon_look_larger_at_the_horizon_we_asked_a_nasa_expert}")
         }
 
         return view
     }
 }
+
+
+
