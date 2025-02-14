04a16066da24e581a37ea0efc6d0e419958d5a59
Oleg Novikov <novikov7@yahoo.com>
2025-02-14 14:39:46 +0300

    Review fixes

--- app/src/main/java/com/example/nasaapp/data/model/NasaResponseDto.kt
+++ app/src/main/java/com/example/nasaapp/data/model/NasaResponseDto.kt
@@ -2,7 +2,6 @@ package com.example.nasaapp.data.model
 
 import com.google.gson.annotations.SerializedName
 
-// DataDto Transfer Object (DTO) для ответа NASA API
 data class NasaResponseDto(
     @SerializedName("collection")
     val collection: CollectionDto
