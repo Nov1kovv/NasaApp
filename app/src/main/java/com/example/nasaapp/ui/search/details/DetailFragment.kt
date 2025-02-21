4db1d95311c6f09786d2e0f075f0d4203bada830
Oleg Novikov <novikov7@yahoo.com>
2025-02-21 13:20:51 +0300

    NestedScrollView DetailFragment

--- app/src/main/java/com/example/nasaapp/ui/search/details/DetailFragment.kt
+++ app/src/main/java/com/example/nasaapp/ui/search/details/DetailFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.example.nasaapp.R
 import com.example.nasaapp.databinding.DetailFragmentBinding
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -17,6 +19,8 @@ class DetailFragment : Fragment() {
     private var _binding: DetailFragmentBinding? = null
     private val binding get() = _binding!!
     private val detailViewModel: DetailViewModel by viewModel()
+    private lateinit var imageProgressBar: ProgressBar
+    private lateinit var fileInfoProgressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +31,26 @@ class DetailFragment : Fragment() {
         val nasaId = arguments?.getString("nasaId") ?:""
         val description = arguments?.getString("description") ?: ""
         binding.fileDescriptionText.text = "Description: $description"
+        imageProgressBar = binding.root.findViewById(R.id.imageProgressBar)
+        fileInfoProgressBar = binding.root.findViewById(R.id.fileInfoProgressBar)
+
+        imageProgressBar.visibility = View.VISIBLE
+        Glide.with(requireContext())
+            .load(imageUrl)
+            .centerCrop()
+            .into(binding.imageView)
 
         Glide.with(requireContext())
             .load(imageUrl)
             .centerCrop()
             .into(binding.imageView)
 
+        binding.imageView.post{
+            imageProgressBar.visibility = View.GONE
+        }
+
         if (nasaId != null) {
+            fileInfoProgressBar.visibility = View.VISIBLE
             detailViewModel.fetchDetailedInfo(nasaId)
         }
 
@@ -42,6 +59,7 @@ class DetailFragment : Fragment() {
                 detailedItem?.let {
                     binding.fileSizeText.text = "File Size: ${it.fileSize}"
                     binding.fileFormatText.text = "Format: ${it.fileFormat}"
+                    fileInfoProgressBar.visibility = View.GONE
                 }
             }
         }
