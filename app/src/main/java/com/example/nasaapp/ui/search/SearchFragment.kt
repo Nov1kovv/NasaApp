2267661f3c19a6a65c0e637023723eb2e7b1eedb
Oleg Novikov <novikov7@yahoo.com>
2025-05-08 16:51:30 +0300

    LiveData

--- app/src/main/java/com/example/nasaapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/nasaapp/ui/search/SearchFragment.kt
@@ -1,33 +1,23 @@
 package com.example.nasaapp.ui.search
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.PorterDuff
-import androidx.appcompat.widget.SearchView
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nasaapp.R
 import com.example.nasaapp.domain.model.SearchItem
 import com.google.android.material.appbar.MaterialToolbar
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
@@ -46,23 +36,19 @@ class SearchFragment : Fragment() {
         val toolbar: MaterialToolbar = view.findViewById(R.id.toolbar)
         val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
         progressBar = view.findViewById(R.id.progress_bar)
-
         requireActivity().window.statusBarColor = Color.BLUE
-
         setupToolbar(toolbar)
         setupRecyclerView(recyclerView)
 
         return view
     }
 
-    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //если искал до этого что-то, то показываю этот запрос, если значение пустое то показываю mars
-        val lastQuery = searchViewModel.getLastQuery()
-        val queryToSearch = if (lastQuery.isNotEmpty()) lastQuery else "mars"
-            performSearch(queryToSearch)
-
+        searchViewModel.searchResult.observe(viewLifecycleOwner) { items ->
+            adapter.updateItems(items)
+            progressBar.visibility = View.GONE
+        }
     }
 
     private fun setupToolbar(toolbar: MaterialToolbar) {
@@ -73,7 +59,8 @@ class SearchFragment : Fragment() {
         val searchIcon = searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_mag_icon)
         searchIcon.visibility = View.GONE
 
-        val searchTextView = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        val searchTextView =
+            searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
         searchTextView.setTextColor(Color.WHITE)
         searchTextView.setHintTextColor(Color.GRAY)
 
@@ -93,6 +80,7 @@ class SearchFragment : Fragment() {
                     filterDialog.show(childFragmentManager, "filter_dialog")
                     true
                 }
+
                 else -> false
             }
         }
@@ -112,29 +100,14 @@ class SearchFragment : Fragment() {
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
-        adapter = SearchAdapter (emptyList()){ searchItem -> onItemClick(searchItem) }
+        adapter = SearchAdapter(emptyList()) { searchItem -> onItemClick(searchItem) }
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
-        }
-
+    }
 
 
     private fun performSearch(query: String) {
-        progressBar.visibility = View.VISIBLE
-            //disposables используется для управления подписками
-        disposables.clear()//очищаю старые подписки
-        val disposable = searchViewModel.searchImages(query, selectedMediaType)//запрос к API через viewmodel
-            .subscribeOn(Schedulers.io()) // выполняем в фоновом потоке, чтобы не заблокировать главный поток
-            .observeOn(AndroidSchedulers.mainThread()) // результат обрабатываю на главном
-            .subscribe({ items -> //подписка не результат
-                progressBar.visibility = View.GONE
-                adapter.updateItems(items)//обновляю адаптер новыми элементами которые пришли с сервера
-            }, { error ->
-                progressBar.visibility = View.GONE
-                Log.e("SearchFragment", "Error ${error.message}")
-            })
-
-        disposables.add(disposable)//добавляю disposable в контейнер CompositeDisposable, чтобы потом управлять подписками и очистить их все сразу, если нужно будет
+        searchViewModel.searchImages(query, selectedMediaType)
     }
 
 
@@ -148,6 +121,7 @@ class SearchFragment : Fragment() {
         }
         findNavController().navigate(R.id.action_searchFragment_to_detailFragment, bundle)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         disposables.clear()
--- app/src/main/java/com/example/nasaapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/nasaapp/ui/search/SearchViewModel.kt
@@ -2,30 +2,39 @@ package com.example.nasaapp.ui.search
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.example.nasaapp.domain.model.SearchItem
 import com.example.nasaapp.domain.repository.NasaRepository
-import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.coroutines.flow.Flow
+import io.reactivex.schedulers.Schedulers
 
 class SearchViewModel(private val repository: NasaRepository) : ViewModel() {
-    private val searchResult = MutableLiveData<List<SearchItem>>()
-    private var lastQuery: String = "" //сохраняю последний запрос пользователя
+    val searchResult = MutableLiveData<List<SearchItem>>()
+    private val errorMessage = MutableLiveData<String?>()
     private val compositeDisposable = CompositeDisposable()
 
-    fun searchImages(query: String, mediaType: String): Single<List<SearchItem>> { //Single - возвращает одно значение либо ошибку
-        lastQuery = query //сохраняею текущий запрос, чтобы потом его можно было повторно использовать
-        return repository.searchImages(query, mediaType)
-}
-    fun getLastQuery(): String = lastQuery //геттер возвращающий последний введённый запрос
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.clear()
+    init {
+        searchImages("nasa", mediaType = "image")
     }
-}
+
+    fun searchImages(
+        query: String,
+        mediaType: String
+    ) {
+        val disposable = repository.searchImages(query, mediaType)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ items ->
+                searchResult.value = items
+            }, { error ->
+                errorMessage.value = error.message
+            })
+        compositeDisposable.add(disposable)
+    }
+
+
+        override fun onCleared() {
+            super.onCleared()
+            compositeDisposable.clear()
+        }
+    }
